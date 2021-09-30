d6fdd371216d231351923821ffa98bd18278acaa
hjy929 <snowsonic1@naver.com>
2021-09-30 22:55:16 +0900

    second

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
             e.preventDefault();  //초기화되는 것을 막는 구성, fullpage slider에서는 대기시키는 이동공간을 고정화
 
             //console.log("마우스휠 이벤트 발생");
-            //console.log(e);
+            console.log(e);
             //console.log(e.originalEvent.wheelDelta);
             console.log(event.wheelDelta);
             //크롬 브라우저에서는 마우스 휠을 내렸을 때, event.wheelDelta : -120
@@ -55,17 +55,19 @@ $(document).ready(function(){
 
             var moveTo = $(window).scrollTop(); //각 섹션별로 상당으로부터 절대위치를 파악하기 위함
             var elmIndex = $(elm).eq(index); //각 섹션별로 순차적으로 접근
-            console.log(elmIndex);
+            console.log(elmIndex); //마우스 휠 이벤트가 발생한 장소 객체(7개의 섹션 중에서 하나의 섹션에서 이벤트를 발생시킴)
+
             
             if(delta < 0){
                 //console.log("마우스 휠 아래로 내렸다.");
                 try{
                     //마우스 휠을 내리는 시점에서 다음 섹션이 존재한다면
-                    if($(elmIndex).next() != undefined){
+                    if($(elmIndex).next() != undefined){ 
                         moveTo = $(elmIndex).next().offset().top; //다음섹션의 상단 수직방향 절대 위치값
 
                         $(elm).removeClass("active");
-                        $(elmIndex).next().addClass("active");
+                        $(elmIndex).next().addClass("active").addClass("complete");
+                        
                         var $cur_index = $(".box.active").index();  //이동시킨 다음 active라는 클래스가 존재한 곳의 인덱스값을 받아온다.
                         console.log($cur_index);
                         $("header li").removeClass("active");
@@ -73,6 +75,7 @@ $(document).ready(function(){
 
                         $("header").removeClass("show");
 
+                        timeout();
                     }
                 }catch(e){  //시도하는 과정에서 문제점이 발생한 곳은 catch문에서 강제로 잡아버린다. (에러 발생에 대한 예외처리)
                     console.log(e);  //타입에러 파트를 에러로 검사창에 노출시키는 것이 아닌 강제로 숨김처리 => 시스템의 에러도 에러로 발생기키는 것을 막는 기능
@@ -81,33 +84,46 @@ $(document).ready(function(){
 
             }else{
                 //console.log("마우스 휠 위로 올렸다.");
-                try{
+                try{ //시도해라
                 //마우스 휠을 올리는 시점에서 이전 섹션이 존재한다면
-                if($(elmIndex).prev() != undefined){
-                    moveTo = $(elmIndex).prev().offset().top; //이전섹션의 상단 수직방향 절대 위치값
+                    if($(elmIndex).prev() != undefined){
+                        moveTo = $(elmIndex).prev().offset().top; //이전섹션의 상단 수직방향 절대 위치값
 
-                    $(elm).removeClass("active");
-                    $(elmIndex).prev().addClass("active");
-                    var $cur_index = $(".box.active").index();
-                    console.log($cur_index);
-                    $("header li").removeClass("active");
-                    $("header li").eq($cur_index).addClass("active");
+                        $(elm).removeClass("active");
 
-                    $("header").addClass("show");
 
-                }
+                        $(elmIndex).prev().addClass("active").addClass("complete");
+                        
+                        var $cur_index = $(".box.active").index();
+                        console.log($cur_index);
+                        $("header li").removeClass("active");
+                        $("header li").eq($cur_index).addClass("active");
+
+                        $("header").addClass("show");
+
+                        timeout();
+                    }
                 }catch(e){  //시도하는 과정에서 문제점이 발생한 곳은 catch문에서 강제로 잡아버린다. (에러 발생에 대한 예외처리)
                     console.log(e);  //타입에러 파트를 에러로 검사창에 노출시키는 것이 아닌 강제로 숨김처리 => 시스템의 에러도 에러로 발생기키는 것을 막는 기능
                 }
 
 
             }
-            $("html, body").stop().animate({scrollTop : moveTo + "px"}, 500);
+            var timeout = setTimeout(function(){
+                var $complete = $(".box.active").hasClass("complete");
+                if($complete == true){
+                    $(".box.active").removeClass("complete");
+                }else{
+                    $("html, body").animate({scrollTop : moveTo + "px"}, 200);
+                }
+
+            }, 200);
+
 
         });
     });
 
-    //상단 메뉴 클릭시 해당하는 페이지로 이동 
+    //상단 메뉴 클릭시 해당하는 페이지로 이동  (+ section.box에 존재한 active 영역도 변경한다.)
     $("header li").click(function(){
         var $index = $(this).index();
         $("header li").removeClass("active");
@@ -138,12 +154,68 @@ $(document).ready(function(){
         key_num = evt.keyCode;
 
         var $target = $(".box.active").index(); //$(elm + ".active")
-        if(key_num == 40 || key_num == 34){
+
+        console.log($target); //최종적으로 active가 들아간 곳을 찾는 것이 아니라 키보드 이벤트가 발생한 시점에서 active라는 클래스가 들어간 곳의 인덱스번호를 추출
+
+        if(key_num == 40 || key_num == 34){ //하방향키 $pageDown 키보드 눌렀을 때
             try{
-                $("html, body").stop().animate({scrollTop : $(elm).eq($target + 1).offset().top}, 500);
-                $(elm).eq($target + 1).addClass("active");
-                $("header li").removeClass("active");
-                $("header li").eq($target + 1).addClass("active");
+                if($target == $(elm).length - 1){
+
+                }else{
+                    $("html, body").stop().animate({scrollTop : $(elm).eq($target + 1).offset().top}, 500, function(){
+                        $(elm).removeClass("active");
+                        $(elm).eq($target + 1).addClass("active");
+                        $("header li").removeClass("active");
+                        $("header li").eq($target + 1).addClass("active");
+
+                    });
+
+                }
+
+            }catch(evt){
+
+            }
+        }else if(key_num == 38 || key_num == 33){ //상방향키 $pageUp 키보드 눌렀을 때
+            try{
+                if($target == 0){
+                    
+                }else{
+                    $("html, body").stop().animate({scrollTop : $(elm).eq($target - 1).offset().top}, 500, function(){
+                        $(elm).removeClass("active");
+                        $(elm).eq($target - 1).addClass("active");
+                        $("header li").removeClass("active");
+                        $("header li").eq($target - 1).addClass("active");
+
+                    });
+
+                }
+
+
+            }catch(evt){
+
+            }
+        }else if(key_num == 36){ //"Home" 키보드 눌렀을 때 맨처음으로 이동
+            try{
+                $("html, body").stop().animate({scrollTop : $(elm).first().offset().top}, 500, function(){
+                    $(elm).removeClass("active");
+                    $(elm).first().addClass("active");
+                    $("header li").removeClass("active");
+                    $("header li").first().addClass("active");
+
+                });
+
+            }catch(evt){
+
+            }
+        }else if(key_num == 35){  //"End" 키보드 눌렀을 때 맨마지막으로 이동
+            try{
+                $("html, body").stop().animate({scrollTop : $(elm).last().offset().top}, 500, function(){
+                    $(elm).removeClass("active");
+                    $(elm).last().addClass("active");
+                    $("header li").removeClass("active");
+                    $("header li").last().addClass("active");
+
+                });
 
             }catch(evt){
 
@@ -151,6 +223,111 @@ $(document).ready(function(){
         }
 
     });
+
+
+    //모바일 환경하에서 터치기반 - touchstart(최초로 화면을 누른 시점에 발생하는 이벤트), touchend(누른 후 드래그 이후의 손가락을 화면에서 떼는 시점에서 발생하는 이벤트)
+    var $t_start; //최초로 터치한 Y축의 값
+    var $t_end; //드래그 이후 언터치한 Y축의 값
+    var $t_move; //$t_start에서 $t_end로 이동한 값을 계산
+
+    
+    function prev(evt){
+        try{
+            var $target = $(".box.active").index();
+            if($target != 0){
+                $("html, body").stop().animate({scrollTop:$(elm).eq($target - 1).offset().top}, 500, function(){
+                    $(elm).removeClass("active");
+                    $(elm).eq($target - 1).addClass("active");
+                    $("header li").removeClass("active");
+                    $("header li").eq($target - 1).addClass("active");
+                });
+
+            }
+
+        }catch(evt){
+
+        }
+    }
+    
+    function next(evt){
+        try{
+            var $target = $(".box.active").index();
+            if($target != $(elm).length - 1){
+                $("html, body").stop().animate({scrollTop:$(elm).eq($target + 1).offset().top}, 500, function(){
+                    $(elm).removeClass("active");
+                    $(elm).eq($target + 1).addClass("active");
+                    $("header li").removeClass("active");
+                    $("header li").eq($target + 1).addClass("active");
+                });
+
+            }
+
+        }catch(evt){
+
+        }
+    }
+    
+    function touchmove(evt){  //evt는 대입에 대한 변수 역할을 부여
+        console.log(evt);
+        $t_move = $t_start - $t_end;
+        console.log($t_move);
+        //터치의 이동방향이 상단 방향이라면 하단 컨텐츠가 나와야 함: 양수
+        //터치의 이동방향이 하단 방향이라면 상단 컨텐츠가 나와야 함: 음수
+
+        if($t_move > 0){ //터치의 이동방향이 상단 방향이라면 하단 컨텐츠가 나와야 함: 양수
+            next(evt);
+        }else if($t_move < 0){ //터치의 이동방향이 하단 방향이라면 상단 컨텐츠가 나와야 함: 음수
+            prev(evt);
+        }
+
+    }
+    
+    $(elm).on("touchstart", function(event){
+        console.log("터치 시작");
+        //console.log(event);
+        console.log(event.changedTouches[0].pageY);
+        $t_start = event.changedTouches[0].pageY;
+
+    });
+
+    $(elm).on("touchend", function(event){
+        console.log("터치 종료");
+        console.log(event.changedTouches[0].pageY);
+        $t_end = event.changedTouches[0].pageY;
+        
+        touchmove();
+    });
+
+    //마우스 이벤트: mousedown, mouseup
+    var $m_down;
+    var $m_up;
+    var $m_move;
+
+    function mousemove(evt){
+        $m_move = $m_down - $m_up;
+        //$m_move라는 결과값이 양수라면 마우스 클릭한 상태에서 상단으로 드래그를 시도한 상황
+        if($m_move > 20){
+            next(evt);
+        }else if($m_move < -20){
+            prev(evt);
+        }
+    }
+
+    //마우스의 클릭 버튼을 누른 상태
+    $(elm).on("mousedown", function(evt){
+        console.log("마우스다운");
+        console.log(evt.pageY);
+        $m_down = evt.pageY;
+    });
+
+    //마우스의 클릭 버튼을 뗀 상태
+    $(elm).on("mouseup", function(evt){
+        console.log(evt.pageY);
+        $m_up = evt.pageY;
+
+        mousemove();
+    });
+
 
 
 
